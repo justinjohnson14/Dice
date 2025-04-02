eb992c1a579d3420db834e2cd28c62ac6d2d0020
Justin Johnson <justinjohnson14@outlook.com>
2025-04-01 19:48:45 -0700

    Removed Gamestate class for now as its just unnecesarily complicating things. Will add back when i see a need for it

--- dice.py
+++ dice.py
@@ -4,12 +4,54 @@ from tkinter import ttk
 
 
 
-class GameState:
-    def __init__(self, cup=6, roll=None, kd=None, score=0):
-        self.cup = cup
-        self.roll = roll
-        self.keptDice = kd
-        self.score = score
+#class GameState:
+#    def __init__(self, cup=6, roll=None, kd=None, score=0):
+#        self.cup = cup
+#        self.roll = roll
+#        self.keptDice = kd
+#        self.score = score
+
+class App(tk.Frame):
+    def __init__(self, master, gs):
+        super().__init__(master)
+        self.pack()
+
+        self.btn_roll = tk.Button(text='Roll',command=self.roll)
+        self.btn_stop = tk.Button(text='Score and stop',command=self.stop)
+        self.dice1 = tk.Radiobutton(text='dice1',command=self.diceClick)
+        self.dice2 = tk.Radiobutton(text='dice2',command=self.diceClick)
+        self.dice3 = tk.Radiobutton(text='dice3',command=self.diceClick)
+        self.dice4 = tk.Radiobutton(text='dice4',command=self.diceClick)
+        self.dice5 = tk.Radiobutton(text='dice5',command=self.diceClick)
+        self.dice6 = tk.Radiobutton(text='dice6',command=self.diceClick)
+        #Add dice gui elements here. Everytime a dice is selected or unselected you must call the calcScore func in GameState
+
+        self.btn_roll.pack()
+        self.btn_stop.pack()
+        self.dice1.pack()
+        self.dice2.pack()
+        self.dice3.pack()
+        self.dice4.pack()
+        self.dice5.pack()
+        self.dice6.pack()
+
+        #self.contents = tk.StringVar()
+
+        #self.contents.set('this is a variable')
+
+        #self.entrythingy["textvariable"] = self.contents
+
+    def print_contents(self, event):
+        print("Hi. The current entry content is:",self.contents.get())
+
+    def roll(self):
+        print('You chose to roll again')
+
+    def stop(self):
+        print('You chose to score and pass')
+
+    def diceClick(self):
+        print('You clicked a dice')
 
     def rollDice(self):
         self.roll = [random.randint(1,6) for x in range(0,self.cup)]
@@ -60,33 +102,6 @@ class GameState:
     def scoreAndPass(self, score):
         self.score += score
         return
-
-
-class App(tk.Frame):
-    def __init__(self, master, gs):
-        super().__init__(master)
-        self.pack()
-
-        self.btn_roll = tk.Button(text='Roll',command=self.roll)
-        self.btn_stop = tk.Button(text='Score and stop',command=self.stop)
-
-        self.btn_roll.pack()
-        self.btn_stop.pack()
-
-        #self.contents = tk.StringVar()
-
-        #self.contents.set('this is a variable')
-
-        #self.entrythingy["textvariable"] = self.contents
-
-    def print_contents(self, event):
-        print("Hi. The current entry content is:",self.contents.get())
-
-    def roll(self):
-        print('You chose to roll again')
-
-    def stop(self):
-        print('You chose to score and pass')
 
 if __name__ == '__main__':
     root = tk.Tk()
